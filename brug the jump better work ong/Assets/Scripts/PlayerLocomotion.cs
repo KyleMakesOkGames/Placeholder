a623450a64feb923e544dadc0ede873bd0d7d17e
KyleMakesGames <95318041+KyleMakesOkGames@users.noreply.github.com>
2022-02-08 15:48:51 -0500

    First Commit

---
+++ brug the jump better work ong/Assets/Scripts/PlayerLocomotion.cs
@@ -0,0 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    PlayerManager playerManager;
+    InputManager inputManager;
+    AnimatorManager animatorManager;
+    Vector3 moveDirection;
+    Transform cameraObject;
+    Rigidbody playerRigidbody;
+
+    public LayerMask groundLayer;
+
+    [Header("Falling Stats")]
+    public float inAirTimer;
+    public float leapingVelocity;
+    public float fallingSpeed;
+    public float rayCastHeightOffSet;
+
+    [Header("Movement Flags")]
+    public bool isGrounded;
+    public bool isSprinting;
+    public bool isJumping;
+
+    [Header("Movement Speeds")]
+    public float walkingSpeed = 1.5f;
+    public float runningSpeed = 5;
+    public float sprintingSpeed = 7;
+    public float rotationSpeed = 15;
+
+    [Header("Jump Speeds")]
+    public float jumpHeight;
+    public float gravityIntensity;
+
+    private void Awake()
+    {
+        playerManager = GetComponent<PlayerManager>();
+        animatorManager = GetComponentInChildren<AnimatorManager>();
+        inputManager = GetComponent<InputManager>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        cameraObject = Camera.main.transform;
+    }
+
+    public void HandleAllMovement()
+    {
+        HandleFallingAndLanding();
+
+        if (playerManager.isInteracting)
+            return;
+
+        if (isJumping)
+            return;
+
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement()
+    {
+        if (isJumping)
+            return;
+
+        moveDirection = cameraObject.forward * inputManager.verticalInput;
+        moveDirection = moveDirection + cameraObject.right * inputManager.horizontalInput;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+
+        if (isSprinting)
+        {
+            moveDirection = moveDirection * sprintingSpeed;
+        }
+        else
+        {
+            if (inputManager.moveAmount >= 0.5f)
+            {
+                moveDirection = moveDirection * runningSpeed;
+            }
+            else
+            {
+                moveDirection = moveDirection * walkingSpeed;
+            }
+        }
+
+        Vector3 movementVelocity = moveDirection;
+        playerRigidbody.velocity = movementVelocity;
+    }
+
+    private void HandleRotation()
+    {
+        if (isJumping)
+            return;
+
+        Vector3 targetDirection = Vector3.zero;
+
+        targetDirection = cameraObject.forward * inputManager.verticalInput;
+        targetDirection = targetDirection + cameraObject.right * inputManager.horizontalInput;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+
+        if (targetDirection == Vector3.zero)
+            targetDirection = transform.forward;
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;
+    }
+
+    private void HandleFallingAndLanding()
+    {
+        RaycastHit hit;
+        Vector3 rayCastOrigin = transform.position;
+        rayCastOrigin.y = rayCastOrigin.y + rayCastHeightOffSet;
+
+        if(!isGrounded && !isJumping)
+        {
+            if(!playerManager.isInteracting)
+            {
+                animatorManager.PlayTargetAnimation("Falling", true);
+            }
+
+            inAirTimer = inAirTimer + Time.deltaTime;
+            playerRigidbody.AddForce(transform.forward * leapingVelocity);
+            playerRigidbody.AddForce(-Vector3.up * fallingSpeed * inAirTimer);
+        }
+
+        if(Physics.SphereCast(rayCastOrigin, 0.2f, -Vector3.up, out hit, groundLayer))
+        {
+            if(!isGrounded && playerManager.isInteracting)
+            {
+                animatorManager.PlayTargetAnimation("Land", true);
+            }
+
+            inAirTimer = 0;
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    public void HandleJumping()
+    {
+        animatorManager.animator.SetBool("isJumping", true);
+        animatorManager.PlayTargetAnimation("Jump", false);
+
+        float jumpingVelociy = Mathf.Sqrt(-2 * gravityIntensity * jumpHeight);
+        Vector3 playerVelocity = moveDirection;
+        playerVelocity.y = jumpingVelociy;
+        playerRigidbody.velocity = playerVelocity;
+    }
+}
---
+++ brug the jump better work ong/Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    InputManager inputManager;
+    PlayerLocomotion playerLocomotion;
+    Animator animator;
+    public bool isInteracting;
+
+    private void Awake()
+    {
+        animator = GetComponentInChildren<Animator>();
+        inputManager = GetComponent<InputManager>();
+        playerLocomotion = GetComponent<PlayerLocomotion>();
+    }
+
+    private void Update()
+    {
+        inputManager.HandleAllInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        playerLocomotion.HandleAllMovement();
+    }
+
+    private void LateUpdate()
+    {
+        isInteracting = animator.GetBool("isInteracting");
+        playerLocomotion.isJumping = animator.GetBool("isJumping");
+        animator.SetBool("isGrounded", playerLocomotion.isGrounded);
+    }
+}
